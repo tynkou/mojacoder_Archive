6bfefcdc39934820c1e2534b3120947bd7e82528
tynkou <q7fhtcdgm5@privaterelay.appleid.com>
2026-09-25 20:30:50 +0900

    Update cop.py

--- cop/cop.py
+++ cop/cop.py
@@ -1,8 +1,9 @@
-a,b=map(int,input().split())
-c=str(b)
-d=int(c[1])
-e=int(c[0])
-ans1=a*d
-ans2=a*e
-ans3=a*b
-print(ans1,ans2,ans3)
+N,O,P=map(int,input().split())
+K=int(input())
+M=list(map(int,input().split()))
+H=list(map(int,input().split()))
+ans=0
+for x in H:
+  if not(x in M):
+     ans+=K     
+print(ans)
